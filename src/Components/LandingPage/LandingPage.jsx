d5ba3e978c33728dab273755b5f875cfdf806ac4
Kristian Utsar <utsaru@gmail.com>
2021-11-07 19:31:09 +0000

    slider

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+
 import LandingFooter from "../LandingFooter/LandingFooter";
 import LandingNav from "../LandingNav/LandingNav";
+import ScrollComponent from "../ScrollComponent/ScrollComponent";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <>
       <LandingNav />
+      <ScrollComponent />
+
       <LandingFooter />
     </>
   );
