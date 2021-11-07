dffe4198f8aff4815cd1714ad224de94395298f6
Kristian Utsar <utsaru@gmail.com>
2021-11-07 19:30:56 +0000

    slider

---
+++ src/Components/ScrollComponent/ScrollComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ScrollComponent.css";
+
+const ScrollComponent = () => {
+  return (
+    <>
+      <div className="scrollWrapper">
+        <div className="slider">
+          <div className="inner">
+            <div className="items"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ScrollComponent;
