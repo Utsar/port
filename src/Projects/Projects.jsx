5a7c2e9237b241131c025e6f97d43a61dd7d30c4
Kristian Utsar <utsaru@gmail.com>
2021-11-09 21:11:24 +0000

    adds projects

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Projects.css";
+import picture3 from "../Assets/whatsapp.PNG";
+import picture2 from "../Assets/ge.PNG";
+import picture1 from "../Assets/photo.jpg";
 
 const Projects = () => {
   return (
@@ -7,14 +10,30 @@ const Projects = () => {
       <div className="projectsWrapper">
         <div class="projectContainer">
           <ul class="hs no-scrollbar">
-            <li class="item">test</li>
-            <li class="item">test</li>
-            <li class="item">test</li>
-            <li class="item">test</li>
-            <li class="item">test</li>
-            <li class="item">test</li>
-            <li class="item">test</li>
-            <li class="item">test</li>
+            <li class="item">
+              <img src={picture2} alt="picture"></img>
+            </li>
+            <li class="item">
+              <img src={picture3} alt="picture"></img>
+            </li>
+            <li class="item">
+              <img src={picture1} alt="picture"></img>
+            </li>
+            <li class="item">
+              <img src={picture1} alt="picture"></img>
+            </li>
+            <li class="item">
+              <img src={picture1} alt="picture"></img>
+            </li>
+            <li class="item">
+              <img src={picture1} alt="picture"></img>
+            </li>
+            <li class="item">
+              <img src={picture1} alt="picture"></img>
+            </li>
+            <li class="item">
+              <img src={picture1} alt="picture"></img>
+            </li>
           </ul>
         </div>
       </div>
