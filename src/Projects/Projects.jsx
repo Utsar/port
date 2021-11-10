ee599f6a1521beb94f4a165883492454e8bb10a8
Kristian Utsar <utsaru@gmail.com>
2021-11-10 22:59:46 +0000

    adds

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -5,16 +5,28 @@ import picture2 from "../Assets/ge.PNG";
 import picture1 from "../Assets/photo.jpg";
 
 const Projects = () => {
+  const handleClick1 = (e) => {
+    e.preventDefault();
+    window.open("https://global-event.vercel.app/", "_blank");
+  };
+  const handleClick2 = (e) => {
+    e.preventDefault();
+    window.open(
+      "https://whatsapp-clone-4ad35ecu6-utsar.vercel.app/login",
+      "_blank"
+    );
+  };
+
   return (
     <>
       <div className="projectsWrapper">
         <div class="projectContainer">
           <ul class="hs no-scrollbar">
             <li class="item">
-              <img src={picture2} alt="picture"></img>
+              <img src={picture2} alt="picture" onClick={handleClick1}></img>
             </li>
             <li class="item">
-              <img src={picture3} alt="picture"></img>
+              <img src={picture3} alt="picture" onClick={handleClick2}></img>
             </li>
             <li class="item">
               <img src={picture1} alt="picture"></img>
